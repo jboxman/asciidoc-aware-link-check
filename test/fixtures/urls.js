e0e76c92673d44c0472bcb189342bf996a53a3fd
Jason Boxman <jasonb@edseek.com>
2020-03-27 22:19:14 -0400

    Add fixtures for testing

--- test/fixtures/urls.js
+++ test/fixtures/urls.js
@@ -1,4 +1,27 @@
-const urls = `
+const createUrlCatalog = require('../../lib/url-catalog');
+
+function testWithUrls(urls = []) {
+  return function testExtractor() {
+    const catalog = createUrlCatalog();
+
+    urls.forEach((url, idx) => {
+      catalog.addUrl(
+        url,
+        {
+          linkType: '<test>',
+          linkText: 'test',
+          sourceFile: __filename,
+          sourceDirectory: __dirname,
+          lineNumber: idx
+        });
+    });
+
+    return catalog;
+  }
+}
+
+const ignoreUrls = testWithUrls(
+`
 https://www.example.net/achiever.php?boundary=airport
 http://birthday.example.org/bomb.html?acoustics=apparel&army=bite
 https://www.example.org/beef/basketball.htm?appliance=boy&bead=belief
@@ -8,6 +31,8 @@ https://brass.example.com/
 http://www.example.com/?air=baby&bead=airplane
 http://www.example.com/book.html#airport
 http://example.com/beds/argument
-https://www.example.com/`.split('\n');
+https://www.example.com/`.split('\n'));
 
-module.exports = urls;
+module.exports = {
+  ignoreUrls
+};
