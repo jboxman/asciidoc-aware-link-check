aef0218291860bb954ae8b18a4d511ace2e281c7
Jason Boxman <jasonb@edseek.com>
2020-03-27 22:18:07 -0400

    Add testing utils

---
+++ test/util.js
@@ -0,0 +1,30 @@
+function logger() {
+  const logs = {
+    stdout: [],
+    stderr: []
+  };
+
+  return {
+    log(v) {
+      return logs['stdout'].push(format(v, ...Array.from(arguments).slice(1)));
+    },
+
+    error(v) {
+      return logs['stderr'].push(format(v, ...Array.from(arguments).slice(1)));
+    },
+
+    getLog(key) {
+      return logs[key];
+    },
+
+    reset() {
+      for(const key of ['stdout', 'stderr']) {
+        logs[key] = [];
+      }
+    }
+  }
+}
+
+module.exports = {
+  logger
+};
